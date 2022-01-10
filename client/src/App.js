aa88b8a50a9539d5f60144611fc5bfbd568e4aa4
Guillem Ferrer <guillemfd@gmail.com>
2022-01-10 22:18:08 +0100

    merging v3

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+
+        <p>
+          Edit {process.env.REACT_APP_maiki} {process.env.REACT_APP_MYKEY} {process.env.REACT_APP_MYKEY2} {process.env.REACT_APP_PORT} <code>src/App.js</code> and save to reload.
+        </p>
+    
+    </div>
+  );
+}
+
+export default App;
