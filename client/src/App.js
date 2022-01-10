8820235a891cf897382d97a0f845a112d98d52a5
Guillem Ferrer <guillemfd@gmail.com>
2022-01-10 23:31:15 +0100

    cleaning client & hidding API_KEY

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="">
 
         <p>
-          Edit {process.env.REACT_APP_maiki} {process.env.REACT_APP_MYKEY} {process.env.REACT_APP_MYKEY2} {process.env.REACT_APP_PORT} <code>src/App.js</code> and save to reload.
+          Edit <strong>{process.env.REACT_APP_MYKEY}</strong> <code>src/App.js</code> and save to reload.
         </p>
     
     </div>
--- server/routes/movies.routes.js
+++ server/routes/movies.routes.js
@@ -11,16 +11,13 @@ router.get("/", (req, res, next) => res.json("NO VALGO PARA NADA"))
 //això va a http://localhost:5005/api/movies/mostPopular
 router.get("/mostPopular", async (req, res, next) => {
     try{
-      const axiosCall = await axios(`https://api.themoviedb.org/3/movie/popular?api_key=147b9c6e5e6d4e77350febe714463b30`)
+      const axiosCall = await axios(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
       const results = axiosCall.data.results
-
       res.json(results);
     }
     catch(err){
       console.log(err)
     }
-
-
 
 //---- OPCIÓN B ------
 
@@ -28,19 +25,6 @@ router.get("/mostPopular", async (req, res, next) => {
     // // .then(data => data.results)
     // console.log('F')
     // .catch(err => console.log(err))
-
-
-//---- OPCIÓN try + catch ------ workiiiiiing!!!!!!!!!!!  <============  <================  <=======
-// router.get("/mostPopular", async (req, res, next) => {
-//     try{
-//       const axiosCall = await axios(`https://api.themoviedb.org/3/movie/popular?api_key=147b9c6e5e6d4e77350febe714463b30`)
-//       const results = axiosCall.data.results
-
-//       res.json(results);
-//     }
-//     catch(err){
-//       console.log(err)
-//     }
   
 })
 
