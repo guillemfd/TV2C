ef30dd601dcd12ac4d9c05641625053011595d36
Guillem Ferrer <guillemfd@gmail.com>
2022-01-12 01:57:55 +0100

    Background Image at HomePage

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -9,8 +9,8 @@ function HomePage() {
             <div id="HomePageBackGround">
                 <img src={BackGroundIMG} alt="HomePageBackGround"></img>
             </div>
-            <h1>Welcome to TV2C app</h1>
-            <h5>Here you can make lists of what are you wishing to watch, update your "To See" list or just look for new moives & TV Series</h5>
+            <h1 style={{color: 'white', textShadow: '2px 2px #000000', fontSize: '80px'}}>Welcome to TV2C app</h1>
+            <h5 style={{color: 'black', textShadow: '1px 1px #ff0000'}}>Here you can make lists of what are you wishing to watch, update your "To See" list or just look for new moives & TV Series</h5>
             <hr/>
             <Link to="/mostPopular">
                 <Button variant="dark" size="xl">Let'save a look!</Button>
